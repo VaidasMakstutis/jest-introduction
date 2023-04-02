77b86a0757d93bce1c6e3712cfb15bac2beeb35a
Vaidas <vaidas.makstutis@gmail.com>
2023-04-02 20:16:14 +0300

    jest hooks

--- calculator.test.js
+++ calculator.test.js
@@ -90,3 +90,32 @@ describe("Calculator tests", () => {
     expect(string1).not.toMatch(/abc/);
   });
 });
+
+describe("Calculator tests", () => {
+  let input1 = 0;
+  let input2 = 0;
+
+  beforeAll(() => {
+    console.log("beforeAll called");
+  });
+
+  afterAll(() => {
+    console.log("afterAll called");
+  });
+  beforeEach(() => {
+    console.log("beforeEach called");
+    input1 = 1;
+    input2 = 2;
+  });
+  afterEach(() => {
+    console.log("afterEach called");
+  });
+
+  test("adding 1 + 2 should return 3", () => {
+    // arrange and act
+    let result = mathOperations.sum(input1, input2);
+
+    // assert
+    expect(result).toBe(3);
+  });
+});
